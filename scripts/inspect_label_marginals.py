cb5590672356b061d1280f14fb38a5377ddde438
Mila Marcheva <mmm67@cl.cam.ac.uk>
2025-11-15 18:33:58 +0000

    Allow variable length in printing

--- scripts/inspect_label_marginals.py
+++ scripts/inspect_label_marginals.py
@@ -169,17 +169,17 @@ def _summarise_spans(
     length = marginals.size(0)
     entries = []
     if 1:
-        max_vals = [[0 for i in range(5)] for j in range(5)]
-        min_vals = [[0 for i in range(5)] for j in range(5)]
-        sum_vals = [[0 for i in range(5)] for j in range(5)]
-        for i in range(5):
-            for j in range(5):
+        max_vals = [[0 for i in range(length)] for j in range(length)]
+        min_vals = [[0 for i in range(length)] for j in range(length)]
+        sum_vals = [[0 for i in range(length)] for j in range(length)]
+        for i in range(length):
+            for j in range(length):
                 for k in range(512):
                     max_vals[i][j] = max(max_vals[i][j],marginals[i,j,k])
                     min_vals[i][j] = min(min_vals[i][j],marginals[i,j,k])
                     sum_vals[i][j] = sum_vals[i][j] + marginals[i,j,k]
-        for i in range(5):
-            for j in range(5):
+        for i in range(length):
+            for j in range(length):
                 if j+1-i>0:
                     print(f'[{i+1}, {j+1}] width={j+1-i}, max_p = {max_vals[i][j]}, min_p = {min_vals[i][j]}, sum_p = {sum_vals[i][j]}')
     max_seen = 0.0
